d8cbab033fba3a927499e3fdbd4851f78c5ed963
perblawiik <31038499+perblawiik@users.noreply.github.com>
2020-03-06 10:17:50 +0100

    Implemented a function for merging top wordlists of all subreddits and added code documentation.

--- server/application.py
+++ server/application.py
@@ -1,7 +1,6 @@
 # Import packages
 from flask import Flask, render_template, jsonify
 from flask_cors import CORS
-import pandas as pd
 import json
 
 import sqlite3
@@ -32,7 +31,7 @@ data = None
 # Derived from python docs. Source: https://docs.python.org/2/library/sqlite3.html#sqlite3.Connection.row_factory
 def dict_factory(cursor, row):
     """ A callback function for formatting the selected rows as dictionaries
-        for easier jsonify
+        for easier jsonify.
     :param cursor: The Cursor object
     :param row: The row object and its parameters
     :return: A dictionary object
@@ -44,9 +43,9 @@ def dict_factory(cursor, row):
 
 # Function derived from SQLite Tutorial. Source: https://www.sqlitetutorial.net/sqlite-python/sqlite-python-select/
 def create_connection(database_file):
-    """ create a database connection to the SQLite database
-        specified by the database_file
-    :param database_file: database file
+    """ Create a database connection to the SQLite database
+        specified by a file path.
+    :param database_file: Database file
     :return: Connection object or None
     """
     connection = None
@@ -59,8 +58,7 @@ def create_connection(database_file):
 
 # Function derived from SQLite Tutorial. Source: https://www.sqlitetutorial.net/sqlite-python/sqlite-python-select/
 def select_data(connection, conditions):
-    """
-    Query all rows in the posts table
+    """ Query all rows in the posts table.
     :param connection: The Connection object
     :param subreddit: The name of the subreddit of which data to select
     :return: The selected data
@@ -71,11 +69,10 @@ def select_data(connection, conditions):
     return cursor.fetchall()
 
 def get_top_words(documents, num_words):
-    """
-    Create a bag of words by counting all words in the documents
+    """ Create a bag of words by counting all words in the documents.
     :param documents: An array of all comments of a subreddit
     :param num_words: Number of words to return
-    :return: the selected data
+    :return: A list of top n most common words
     """
     # The vocabulary (Bag of Words) containing all unique words and their frequency
     vocabulary = {}
@@ -97,6 +94,97 @@ def get_top_words(documents, num_words):
     # Get the top n most common words
     return heapq.nlargest(num_words, vocabulary.items(), key=itemgetter(1))
 
+def merge_top_words(nodes, num_words):
+    """ Create a bag of words by merging all word counts in the node word lists.
+    :param nodes: An array of subreddits with corresponding word lists
+    :param num_words: Number of words to return
+    :return: A list of top n most common words
+    """
+    total_top_words = {}
+    for node in nodes:
+        for word_object in node['words']:
+            word = word_object['word']
+            if word not in total_top_words.keys():
+                total_top_words[word] = [word_object['amount'], word_object['score']]
+            else:
+                total_top_words[word][0] += word_object['amount']
+                total_top_words[word][1] += word_object['score']
+    
+    # Get the top n most common words
+    return heapq.nlargest(num_words, total_top_words.items(), key=itemgetter(1))
+
+def create_simularity_matrix(term_documents):
+    """ Make a term frequency–inverse document frequency matrix 
+        from given documents, and compute cosine simularities.
+    :param term_documents: A list of documents
+    :return: A matrix holding cosine simularities between the documents
+    """
+    # Create a sparse document matrix with the word counts
+    vectorizer = CountVectorizer()
+    count_matrix = vectorizer.fit_transform(term_documents)
+
+    # Transform the word counts matrix to a tf idf matrix
+    tfidf_transformer = TfidfTransformer()
+    tfidf_matrix = tfidf_transformer.fit_transform(count_matrix)
+
+    # Compute document simularities between the different subreddits
+    return cosine_similarity(tfidf_matrix)
+
+def create_simularity_links(simularity_matrix, num_subreddits, tolerance):
+    """ Make a list of a link system between different subreddits
+        based on the cosine simularity.
+    :param simularity_matrix: A matrix holding cosine simularities
+    :param num_subreddits: The total number of subreddits
+    :param tolerance: A tolerance threshold for links
+    :return: A list of the linked system
+    """
+    # Create an undirected linked graph system
+    links = []
+    for source in range (num_subreddits):
+        # Let the simularities be weights
+        weights = simularity_matrix[source]
+
+        # Compare the weights between the source and targets
+        for target in range(source+1, num_subreddits):
+            if weights[target] > tolerance:
+                links.append({'source':subreddit_list[source],'target':subreddit_list[target], 'value':weights[target]})
+
+    return links           
+
+                
+def cluster_data(simularity_matrix, num_clusters):
+    """ Apply hierarchical clustering based on the cosine distance
+        between documents.
+    :param simularity_matrix: A matrix holding cosine simularities
+    :param num_clusters: The total number of clusters
+    :return: A list of group ids connected to documents (subreddits in our case)
+    """
+    # Distances between documents
+    dist = 1 - simularity_matrix
+
+    # Apply hierarchical clustering using ward linkage
+    cluster = AgglomerativeClustering(n_clusters=num_clusters, affinity='euclidean', linkage='ward')
+    groups = cluster.fit_predict(dist)
+
+    # Print the group hierarchy to console
+    #print_cluster_grouping(groups, num_clusters)
+        
+    return groups
+
+def print_cluster_grouping(groups, num_clusters):
+    """ Prints all groups with their subreddits to the console
+        for analysing purpose.
+    :param groups: A list of group ids connected to the documents
+    :param num_clusters: The total number of clusters
+    """
+    group_index = 0
+    for group_id in range(num_clusters):
+        print("\nGroup " + str(group_id) + " : ")
+
+        for index in range(len(groups)):
+            if groups[index] == group_id:
+                print(subreddit_list[index])
+
 def data_mine():
     # Create a connection to the database
     database = r"C:\Users\persj\Desktop\InfoVis\reddit-comments-may-2015\processed_database.db"
@@ -110,6 +198,7 @@ def data_mine():
         # Set the row format to dictionary
         connection.row_factory = dict_factory
 
+        # Process one subreddit at a time
         for name in subreddit_list:
             # Select all data from specified subreddit
             conditions = "subreddit='"+name+"'"
@@ -130,10 +219,13 @@ def data_mine():
             # Save the subreddit as a node
             nodes.append({'id':name, 'size':len(posts), 'group': 0, 'words':words})
     
-    print("Word frequency vocabularies complete!")
+    # Merge word lists for each node and return top 50 words
+    total_top_words = merge_top_words(nodes, 50)
+    words = []
+    for word_object in total_top_words:
+        words.append({'word':word_object[0],'amount':word_object[1][0], 'score':word_object[1][1]})
 
-    import time
-    start = time.time()
+    print("Word frequency vocabularies complete!")
 
     # Compute document simularities between the subreddits
     simularity_matrix = create_simularity_matrix(term_documents)
@@ -151,10 +243,7 @@ def data_mine():
         nodes[index]['group'] = int(groups[index])
 
     global data
-    data = {'nodes':nodes, 'links':links}  
-
-    end = time.time()
-    print("Elapsed time: " + str(end - start))
+    data = {'nodes':nodes, 'links':links, 'words':words}  
 
     # Save data to a file
     with open('data.json', 'w') as outfile:
@@ -162,56 +251,6 @@ def data_mine():
         json.dump(data, outfile)
 
     print("Data mining complete!")
-    
-
-def create_simularity_matrix(term_documents):
-    # Create a sparse document matrix with the word counts
-    vectorizer = CountVectorizer()
-    count_matrix = vectorizer.fit_transform(term_documents)
-
-    # Transform the word counts matrix to a tf idf matrix
-    tfidf_transformer = TfidfTransformer()
-    tfidf_matrix = tfidf_transformer.fit_transform(count_matrix)
-
-    # Compute document simularities between the different subreddits
-    return cosine_similarity(tfidf_matrix)
-
-def create_simularity_links(simularity_matrix, num_subreddits, tolerance):
-    # Create an undirected linked graph system
-    links = []
-    for source in range (num_subreddits):
-        # Let the simularities be weights
-        weights = simularity_matrix[source]
-
-        # Compare the weights between the source and targets
-        for target in range(source+1, num_subreddits):
-            if weights[target] > tolerance:
-                links.append({'source':subreddit_list[source],'target':subreddit_list[target], 'value':weights[target]})
-
-    return links           
-
-                
-def cluster_data(simularity_matrix, num_clusters):
-    # Distances between documents
-    dist = 1 - simularity_matrix
-
-    # Apply hierarchical clustering using ward linkage
-    cluster = AgglomerativeClustering(n_clusters=num_clusters, affinity='euclidean', linkage='ward')
-    groups = cluster.fit_predict(dist)
-
-    # Print the group hierarchy to console
-    #print_cluster_grouping(groups, num_clusters)
-        
-    return groups
-
-def print_cluster_grouping(groups, num_clusters):
-    group_index = 0
-    for group_id in range(num_clusters):
-        print("\nGroup " + str(group_id) + " : ")
-
-        for index in range(len(groups)):
-            if groups[index] == group_id:
-                print(subreddit_list[index])
 
 # Instantiate the application
 app = Flask(__name__, template_folder='../app')
