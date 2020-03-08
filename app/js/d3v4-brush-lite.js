f530d22685ebcab7a308cab2357971fa4e611d43
Johanna Palmkvist <johpa016@student.liu.se>
2020-03-09 00:32:10 +0100

    fixed select controls

--- app/js/d3v4-brush-lite.js
+++ app/js/d3v4-brush-lite.js
@@ -119,7 +119,7 @@ function defaultFilter() {
 
 function defaultExtent() {
   var svg = this.ownerSVGElement || this;
-  return [[0, 0], [svg.width.baseVal.value, svg.height.baseVal.value]];
+  return [[0, 0], [1365, 722]];
 }
 
 // Like d3.local, but with the name “__brush” rather than auto-generated.
